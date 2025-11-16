1fbac7fa461661108bd3e7f21099010ef815fb4d
David Johnston <davidajohnston2000@gmail.com>
2025-11-16 01:24:52 +0000

    Move `spices.ts` to `routes` directory and update import path in `app.ts`

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import express from 'express';
-import spiceRouter from './api/routes/spices.ts';
+import spiceRouter from './routes/spices.ts';
 
 const app = express();
 
