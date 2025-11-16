45a9727d2f169118f2794dcc123285e46a9e535b
David Johnston <davidajohnston2000@gmail.com>
2025-11-16 01:51:24 +0000

    Return a list of spices in `GET /spices` endpoint instead of a single spice.

--- src/routes/spices.ts
+++ src/routes/spices.ts
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-  res.json({ id: 1, name: 'Cinnamon' });
+  res.json([
+    { id: 1, name: 'Cinnamon' },
+    { id: 2, name: 'Nutmeg' },
+  ]);
 });
 
 export default router;
