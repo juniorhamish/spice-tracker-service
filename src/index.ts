086823507f8e80b7e01464c85beb0c0b18ca65ff
David Johnston <davidajohnston2000@gmail.com>
2025-11-16 01:44:00 +0000

    Add server listening on configured port in `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('/api/spices', spiceRouter);
 
+const port = process.env.PORT ?? '3000';
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
 export default app;
