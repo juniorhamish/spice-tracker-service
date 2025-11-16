4e0121b4919a954b8e0f3a9b10ea1a4f219e87e9
David Johnston <davidajohnston2000@gmail.com>
2025-11-16 01:11:07 +0000

    Reorder imports in `app.ts` for better readability and consistency

--- app.ts
+++ app.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import spiceRouter from './api/routes/spices.ts';
-import helmet from "helmet";
 import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import spiceRouter from './api/routes/spices.ts';
 
 const app = express();
 
 app.use(helmet());
 app.use(
-    cors({
-        origin: [
-            'https://www.dajohnston.co.uk',
-            'http://localhost:5173',
-            'http://localhost:5174',
-            'https://generator.swagger.io',
-            /^https:\/\/.*\.vercel\.app$/,
-        ],
-    }),
+  cors({
+    origin: [
+      'https://www.dajohnston.co.uk',
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://generator.swagger.io',
+      /^https:\/\/.*\.vercel\.app$/,
+    ],
+  }),
 );
 app.use(express.json());
 
@@ -23,7 +23,7 @@ app.use('/api/spices', spiceRouter);
 
 const port = process.env.PORT ?? '3000';
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
 export default app;
