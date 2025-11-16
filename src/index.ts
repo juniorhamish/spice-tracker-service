7f33fc1f11c57e93826bfe6ee35499003d998631
David Johnston <davidajohnston2000@gmail.com>
2025-11-16 01:47:18 +0000

    Simplify `tsconfig.json` and adjust indentation in `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use('/api/spices', spiceRouter);
 
 const port = process.env.PORT ?? '3000';
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
 export default app;
