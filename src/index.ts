ed4aae8f6a6ae898bbf2389593953125cb21ca3e
David Johnston <davidajohnston2000@gmail.com>
2025-11-25 20:38:59 +0000

    Import express to satisfy vercel

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+import express from 'express';
 import createApp, { generalErrorHandler, notFoundHandler } from 'express-shared-lib';
 import spiceRouter from './routes/spices.js';
 
-const app = createApp({
+const app = express();
+const api = createApp({
   jwtAudience: 'https://spice-tracker-service.dajohnston.co.uk',
   includeSpec: true,
 });
 
-app.use('/api/spices', spiceRouter);
-app.use(generalErrorHandler, notFoundHandler);
+api.use('/api/spices', spiceRouter);
+api.use(generalErrorHandler, notFoundHandler);
+app.use(api);
 
 export default app;
